bb3fbf5acb0c588c110d7018448e432b19487759
Thomas Hawley <thawley22@turing.edu>
2023-02-15 10:15:05 -0700

    feat: Add list_all_students method

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,8 +1,20 @@
 class GradeBook
   attr_reader :instructor, :courses
-  
+
   def initialize(instructor)
     @instructor = instructor
     @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    students_by_course = {}
+    @courses.each do |course|
+      students_by_course[course.name] = course.students
+    end
+    students_by_course
   end
 end
