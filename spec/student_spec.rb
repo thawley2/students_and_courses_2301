82c6964e1596ada8b49318ec8f119039809b2f5c
Thomas Hawley <thawley22@turing.edu>
2023-02-15 09:11:41 -0700

    test: Add tests for adding and retrieving scores

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Student do
 
   describe '#initialize' do
     it 'exists' do
-      expect(@studetn).to be_instance_of(Student)
+      expect(@student).to be_instance_of(Student)
     end
 
     it 'has a name' do
@@ -19,4 +19,25 @@ RSpec.describe Student do
       expect(@student.age).to eq(21)
     end
   end
+
+  describe '#scores' do
+    it 'starts with empty scores' do
+      expect(@student.scores).to eq([])
+    end
+
+    it 'can log scores' do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.scores).to eq([89, 78])
+    end
+
+    it 'can return the average of all scores' do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.grade).to eq(83.5)
+    end
+  end
+  
 end
