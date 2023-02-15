0540c7147eaccf25a30f6daa318051e49108848c
Thomas Hawley <thawley22@turing.edu>
2023-02-15 09:37:08 -0700

    test: Add tests for adding students to course

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Course do
 
   before(:each) do
     @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})
   end
 
   describe '#initialize' do
@@ -20,7 +22,38 @@ RSpec.describe Course do
     it 'has a capacity' do
       expect(@course.capacity).to eq(2)
     end
+  end
+
+  describe '#students' do
+    it 'starts with 0 students' do
+      expect(@course.students).to eq([])
+    end
+
+    it 'starts off not full' do
+      expect(@course.full?).to be false
+    end
+
+    it 'can enroll students' do
+      @course.enroll(@student1)
+      @course.enroll(@student2)
+
+      expect(@course.students).to eq([@student1, @student2])
+    end
+
+    it 'becomes full when at capacity' do
+      @course.enroll(@student1)
+      @course.enroll(@student2)
+      
+      expect(@course.full?).to be true
+    end
+
+    it 'cannot enroll more students than capacity' do
+      @course.enroll(@student1)
+      @course.enroll(@student2)
+
+      expect(@course.enroll(@student2)).to eq('No more students can enroll')
+    end
+  end
 
     
-  end
 end
