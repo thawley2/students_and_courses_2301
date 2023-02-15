27b407aac177915fb66081d8d43c7fd35674d2ca
Thomas Hawley <thawley22@turing.edu>
2023-02-15 09:37:41 -0700

    feat: Add methods to enroll students and check if course is full

--- lib/course.rb
+++ lib/course.rb
@@ -1,8 +1,22 @@
 class Course
-  attr_reader :name, :capacity
-  
+  attr_reader :name, :capacity, :students
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @students = []
+    @full = false
   end
+
+  def full?
+    @students.length >= @capacity
+  end
+
+  def enroll(student)
+    if full?
+      'No more students can enroll'
+    else @students << student
+    end
+  end
+  
 end
