f764ba5302198493004c8e7332ccd5db61bc6747
Thomas Hawley <thawley22@turing.edu>
2023-02-15 09:16:56 -0700

    feat: Add methods to log score and retrieve grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,16 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
   def initialize(student_deets)
     @name = student_deets[:name]
     @age = student_deets[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum.fdiv(@scores.length)
   end
 end
